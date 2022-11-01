aff679a356f09a4b380150bce534a94848d8aed6
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2022-11-01 18:59:06 -0300

    style and markup of the hero section

--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -4,14 +4,31 @@
 
 <!-- * TEMPLATE -->
 <template>
-  <section class="hero">
-    We make your music sound extraordinary. A system audio equalizer
-    specifically designed for Android and iOS. Freely tune the way your music
-    sounds with a professional grade parametric EQ & volume mixer. Control bass,
-    mids, treble, gain control, reverb, and more!
+  <section class="hero wrapper">
+    <h1 class="hero__title">We make your music sound extraordinary.</h1>
+    <p class="hero__description">
+      A system audio equalizer specifically designed for Android and iOS. Freely
+      tune the way your music sounds with a professional grade parametric EQ &
+      volume mixer. Control bass, mids, treble, gain control, reverb, and more!
+    </p>
   </section>
 </template>
 
 <!-- * STYLE -->
 <style lang="scss">
+  .hero {
+    margin-bottom: 6.8rem;
+
+    & .hero__title {
+      font-size: 4rem;
+      line-height: 4.8rem;
+      font-weight: 700;
+      letter-spacing: -0.45px;
+      margin-bottom: 2rem;
+    }
+
+    & .hero__description {
+      line-height: 2.6rem;
+    }
+  }
 </style>
